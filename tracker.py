7b8694a7bd4caa90bb8ddd89a0a804df2f8d8a1e
Bhoomika716 <hbhoomi2004@gmail.com>
2026-06-23 12:07:39 +0530

    Scrape price and send alert if below target

--- tracker.py
+++ tracker.py
@@ -1,11 +1,17 @@
-import json
 import os
 import requests
+from bs4 import BeautifulSoup
 
 TELEGRAM_TOKEN = os.environ["TELEGRAM_TOKEN"]
 CHAT_ID = os.environ["TELEGRAM_CHAT_ID"]
 
+URL = "https://dl.flipkart.com/dl/prestige-1600-w-induction-cooktop-push-button/p/itm15d69f3360370"
+
 TARGET_PRICE = 2200
+
+HEADERS = {
+    "User-Agent": "Mozilla/5.0"
+}
 
 def send_message(msg):
     requests.post(
@@ -16,12 +22,21 @@ def send_message(msg):
         }
     )
 
-# Temporary test price
-current_price = 1999
+r = requests.get(URL, headers=HEADERS)
 
-if current_price <= TARGET_PRICE:
-    send_message(
-        f"""🔥 PRICE ALERT
+soup = BeautifulSoup(r.text, "html.parser")
+
+price_tag = soup.find("div", {"class": "Nx9bqj"})
+
+if price_tag:
+    price_text = price_tag.text
+    current_price = int(
+        "".join(c for c in price_text if c.isdigit())
+    )
+
+    if current_price <= TARGET_PRICE:
+        send_message(
+            f"""🔥 PRICE ALERT
 
 Prestige PIC 20 NEO
 
@@ -29,7 +44,6 @@ Current Price: ₹{current_price}
 
 Target Price: ₹{TARGET_PRICE}
 
-Buy Now:
-https://dl.flipkart.com/dl/prestige-1600-w-induction-cooktop-push-button/p/itm15d69f3360370
+{URL}
 """
-    )
+        )
