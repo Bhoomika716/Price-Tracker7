b49307ac2b5ae0508a380e26ec52a6b75326c6bd
Bhoomika716 <hbhoomi2004@gmail.com>
2026-06-23 11:58:45 +0530

    Update price tracking logic and messaging
    Refactor price alert message and Telegram API call.

--- tracker.py
+++ tracker.py
@@ -1,21 +1,35 @@
-import requests
+import json
 import os
+import requests
 
-TOKEN = os.environ["TELEGRAM_TOKEN"]
+TELEGRAM_TOKEN = os.environ["TELEGRAM_TOKEN"]
 CHAT_ID = os.environ["TELEGRAM_CHAT_ID"]
 
-msg = """
-✅ Price Tracker Working
+TARGET_PRICE = 2200
+
+def send_message(msg):
+    requests.post(
+        f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage",
+        data={
+            "chat_id": CHAT_ID,
+            "text": msg
+        }
+    )
+
+# Temporary test price
+current_price = 1999
+
+if current_price <= TARGET_PRICE:
+    send_message(
+        f"""🔥 PRICE ALERT
 
 Prestige PIC 20 NEO
 
-GitHub Action executed successfully.
-"""
+Current Price: ₹{current_price}
 
-requests.post(
-    f"https://api.telegram.org/bot{TOKEN}/sendMessage",
-    data={
-        "chat_id": CHAT_ID,
-        "text": msg
-    }
-)
+Target Price: ₹{TARGET_PRICE}
+
+Buy Now:
+https://dl.flipkart.com/dl/prestige-1600-w-induction-cooktop-push-button/p/itm15d69f3360370
+"""
+    )
